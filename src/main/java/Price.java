54991dbe1d0b7b6930f482ae20eaaac6df23087e
koomin1227 <koomin1227@naver.com>
2026-02-10 12:23:20 +0900

    feat: Price 구현

---
+++ src/main/java/Price.java
@@ -0,0 +1,12 @@
+public class Price {
+    private final Integer value;
+    private static final int PRICE_PER_LOTTO = 1000;
+
+    public Price(Integer price) {
+        this.value = price;
+    }
+
+    public Integer getLottoCount() {
+        return value / PRICE_PER_LOTTO;
+    }
+}
