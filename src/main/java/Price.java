1d25ba15d9c0baa746842110c2f26a17ef6ba1b3
ian.lee2 <ian.lee2@kakaocorp.com>
2026-02-11 14:03:39 +0900

    refactor: Input 검증 로직 Price 로 이동

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -2,8 +2,21 @@ public class Price {
     private final Integer value;
     private static final int PRICE_PER_LOTTO = 1000;
 
-    public Price(Integer price) {
-        this.value = price;
+    public Price(String price) {
+        int parsedValue;
+        try {
+            parsedValue = Integer.parseInt(price);
+
+            if (parsedValue < PRICE_PER_LOTTO) {
+                throw new IllegalArgumentException("구입 금액은 1000원 이상이어야 합니다. 다시 입력해주세요.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("구입 금액은 숫자여야 합니다. 다시 입력해주세요.");
+        }
+
+
+
+        this.value = parsedValue;
     }
 
     public Integer getLottoCount() {
@@ -13,4 +26,6 @@ public class Price {
     public Integer getValue() {
         return value;
     }
+
+
 }
--- src/test/java/PriceTest.java
+++ src/test/java/PriceTest.java
@@ -7,7 +7,7 @@ public class PriceTest {
     @Test
     @DisplayName("가격이 들어왔을때 로또 몇개 반환하는지 반환.")
     void returns_lotto_count () {
-        Price price = new Price(12000);
+        Price price = new Price("12000");
 
         assertThat(price.getLottoCount()).isEqualTo(12);
 
@@ -16,7 +16,7 @@ public class PriceTest {
     @Test
     @DisplayName("1000원 이하면 0개 반환")
     void returns_zero_lotto_count () {
-        Price price = new Price(500);
+        Price price = new Price("500");
 
         assertThat(price.getLottoCount()).isEqualTo(0);
 
@@ -25,7 +25,7 @@ public class PriceTest {
     @Test
     @DisplayName("1000원 단위 이하는 버림")
     void returns_floor_lotto_count () {
-        Price price = new Price(12500);
+        Price price = new Price("12500");
 
         assertThat(price.getLottoCount()).isEqualTo(12);
 
