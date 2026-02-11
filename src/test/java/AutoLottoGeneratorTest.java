e550c188d305597b3ef1141dddbd4b5ac336211c
ian.lee2 <ian.lee2@kakaocorp.com>
2026-02-11 15:46:21 +0900

    refactor: 구현체가 아닌 인터페이스 타입으로 변수 선언

--- src/test/java/AutoLottoGeneratorTest.java
+++ src/test/java/AutoLottoGeneratorTest.java
@@ -1,12 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AutoLottoGeneratorTest {
 
     @Test
     public void 서로_다른_6자리_로또번호_생성() {
-        AutoLottoGenerator autoLottoGenerator = new AutoLottoGenerator();
+        LottoGenerator autoLottoGenerator = new AutoLottoGenerator();
         Lotto lotto = autoLottoGenerator.generateLotto();
 
         assertThat(lotto).isNotNull();
